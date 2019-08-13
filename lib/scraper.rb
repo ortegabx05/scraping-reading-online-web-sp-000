e525809890cb99f7adfff9cd04148448c177216f
jessi ortega <ortegabx05@gmail.com>
2019-08-13 14:58:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-doc.css(".site-header__hero__headline")
+doc.css(".site-header__hero__headline").text
 
 
